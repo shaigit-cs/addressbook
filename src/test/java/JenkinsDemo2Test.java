24b4665a385dd44cf4d12156c19a0e11da13d3a1
shaigit-cs <100363783+shaigit-cs@users.noreply.github.com>
2022-03-11 22:03:33 +0200

    Update JenkinsDemo2Test.java

--- src/test/java/JenkinsDemo2Test.java
+++ src/test/java/JenkinsDemo2Test.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class JenkinsDemo2 {
+public class JenkinsDemo2Test {
 	
 public static void main(String[] args) {
 
