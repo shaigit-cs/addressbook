807e75ed7ddbe3938bcfd26bfaa10d7ed4c31ffa
shaigit-cs <100363783+shaigit-cs@users.noreply.github.com>
2022-03-11 20:52:09 +0200

    Update JenkinsDemoTest.java

--- src/test/java/JenkinsDemoTest.java
+++ src/test/java/JenkinsDemoTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeTest;
 
 public class JenkinsDemoTest{
 public String baseUrl = "https://www.google.com/";
-String driverPath = "chromedriver";
+String driverPath = "/home/shaigtzgmail/Downloads/chromedriver";
 
 public WebDriver driver ;
 
